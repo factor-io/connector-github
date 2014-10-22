f868167068a20ca506d248c9105806c13dd073a7
Andrew Akers <andrewrdakers@gmail.com>
2014-10-22 08:20:59 -0700

    close #9

--- spec/github_issues_spec.rb
+++ spec/github_issues_spec.rb
@@ -5,13 +5,15 @@ describe 'github' do
   before(:all) do
     @api_key   = ENV['GITHUB_APIKEY']
     @username  = 'andrewrdakers'
-    @repo_name = 'working_with_github_api'
+    @repo      = 'working_with_github_api'
     @filter    = 'created'
     @state     = 'open'
     @since     = '2011-04-12T12:12:12Z'
     @labels    = 'enhancement'
     @sort      = 'comments'
     @direction = 'asc'
+    @title     = 'title-' + Random.rand(9999).to_s
+    @body      = 'body-' + Random.rand(9999).to_s
   end
 
   describe 'list' do
@@ -20,7 +22,7 @@ describe 'github' do
       params = {
         'api_key'   => @api_key,
         'username'  => @username,
-        'repo_name' => @repo_name,
+        'repo'      => @repo,
         'filter'    => @filter,
         'state'     => @state,
         'since'     => @since,
@@ -29,6 +31,22 @@ describe 'github' do
         'direction' => @direction
       }
       service_instance.test_action('list', params) do
+        expect_return
+      end
+    end
+  end
+
+  describe 'create' do
+    it 'can create a new issue' do
+      service_instance = service_instance('github_issues')
+      params = {
+        'api_key'  => @api_key,
+        'username' => @username,
+        'repo'     => @repo,
+        'title'    => @title,
+        'body'     => @body
+      }
+      service_instance.test_action('create', params) do
         expect_return
       end
     end
