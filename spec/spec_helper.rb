b679bdceee40251ffcd2c99168cde9e72af0f09a
Maciej Skierkowski <maciej@skierkowski.com>
2015-01-09 15:51:26 -0800

    DRY @api_key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,8 @@ Dir.glob('./lib/factor/connector/*.rb').each { |f| require f }
 
 RSpec.configure do |c|
   c.include Factor::Connector::Test
+
+  c.before do
+    @api_key = ENV['GITHUB_API_KEY']
+  end
 end
